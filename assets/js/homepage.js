16582272ddce4a4ed6759be5033cdf34e18c587e
Mary Margaret <mmlawton15@gmail.com>
2021-12-01 20:28:12 -0500

    error handling with single repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -56,8 +56,9 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
       //format repo name
       var repoName = repos[i].owner.login + "/" + repos[i].name;
       //create a container for ech repo
-      var repoEl=document.createElement("div");
+      var repoEl=document.createElement("a");
       repoEl.classList = "list-item flex-row justify-space-between align-center";
+      repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
       //create a span element to hold repository name
       var titleEl = document.createElement("span");
       titleEl.textContent = repoName;
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,22 @@
 
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to homepage
+        document.location.replace("./index.html");
+    }
+};
+
 
 var displayWarning = function(repo) {
     //add text to warning coontainer
@@ -16,6 +32,7 @@ var displayWarning = function(repo) {
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    //make a get request to url
     fetch(apiUrl).then(function(response) {
         //request was successful
         if (response.ok) {
@@ -29,11 +46,12 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
 
     })
-    console.log(repo)
+
 
 };
 
@@ -69,5 +87,4 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 };
-
-getRepoIssues("facebook/react");
+getRepoName();
